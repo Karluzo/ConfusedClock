febf12b34c4a6427174eb8e52a2bd8495e5fd37f
Karluzo <dohnal.vojtech@gmail.com>
2016-06-03 14:48:46 +0200

    Add files via upload
    Interactive app to solve Confused Clock Face puzzle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox0 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.chkForward = new System.Windows.Forms.CheckBox();
+            this.txtOldIndex = new System.Windows.Forms.TextBox();
+            this.txtNewIndex = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication1.Properties.Resources.clock;
+            this.pictureBox1.Location = new System.Drawing.Point(37, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(370, 360);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox0
+            // 
+            this.textBox0.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox0.Location = new System.Drawing.Point(195, 74);
+            this.textBox0.Name = "textBox0";
+            this.textBox0.ReadOnly = true;
+            this.textBox0.Size = new System.Drawing.Size(51, 29);
+            this.textBox0.TabIndex = 1;
+            this.textBox0.Click += new System.EventHandler(this.textBox0_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(281, 98);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(51, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(325, 142);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(51, 29);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox3.Location = new System.Drawing.Point(346, 216);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(51, 29);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Click += new System.EventHandler(this.textBox3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox4.Location = new System.Drawing.Point(325, 281);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(51, 29);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox5.Location = new System.Drawing.Point(268, 329);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(51, 29);
+            this.textBox5.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox6.Location = new System.Drawing.Point(195, 349);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(51, 29);
+            this.textBox6.TabIndex = 7;
+            this.textBox6.Click += new System.EventHandler(this.textBox6_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox7.Location = new System.Drawing.Point(121, 329);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(51, 29);
+            this.textBox7.TabIndex = 8;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox8.Location = new System.Drawing.Point(78, 288);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(51, 29);
+            this.textBox8.TabIndex = 9;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox9.Location = new System.Drawing.Point(47, 216);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(51, 29);
+            this.textBox9.TabIndex = 10;
+            this.textBox9.Click += new System.EventHandler(this.textBox9_Click);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox10.Location = new System.Drawing.Point(83, 149);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.ReadOnly = true;
+            this.textBox10.Size = new System.Drawing.Size(51, 29);
+            this.textBox10.TabIndex = 11;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox11.Location = new System.Drawing.Point(121, 98);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.ReadOnly = true;
+            this.textBox11.Size = new System.Drawing.Size(51, 29);
+            this.textBox11.TabIndex = 12;
+            // 
+            // chkForward
+            // 
+            this.chkForward.AutoSize = true;
+            this.chkForward.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.chkForward.Location = new System.Drawing.Point(164, 442);
+            this.chkForward.Name = "chkForward";
+            this.chkForward.Size = new System.Drawing.Size(118, 28);
+            this.chkForward.TabIndex = 13;
+            this.chkForward.Text = "ClockWise";
+            this.chkForward.UseVisualStyleBackColor = true;
+            // 
+            // txtOldIndex
+            // 
+            this.txtOldIndex.Location = new System.Drawing.Point(25, 448);
+            this.txtOldIndex.Name = "txtOldIndex";
+            this.txtOldIndex.ReadOnly = true;
+            this.txtOldIndex.Size = new System.Drawing.Size(100, 20);
+            this.txtOldIndex.TabIndex = 14;
+            // 
+            // txtNewIndex
+            // 
+            this.txtNewIndex.Location = new System.Drawing.Point(325, 448);
+            this.txtNewIndex.Name = "txtNewIndex";
+            this.txtNewIndex.ReadOnly = true;
+            this.txtNewIndex.Size = new System.Drawing.Size(100, 20);
+            this.txtNewIndex.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 474);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 31);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Undo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(234, 476);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(191, 31);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Restart";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 518);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNewIndex);
+            this.Controls.Add(this.txtOldIndex);
+            this.Controls.Add(this.chkForward);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox0);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox0;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.CheckBox chkForward;
+        private System.Windows.Forms.TextBox txtOldIndex;
+        private System.Windows.Forms.TextBox txtNewIndex;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        byte[] clockFace;
+        List<byte[]> history;
+        private List<AllowedMove> moves ;
+
+
+        byte? oldIndex = null;
+        byte? newIndex = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Restart();
+            
+        }
+
+        private void RenderClockFace()
+        {
+            textBox0.Text = clockFace[0].ToString();
+            textBox1.Text = clockFace[1].ToString();
+            textBox2.Text = clockFace[2].ToString();
+            textBox3.Text = clockFace[3].ToString();
+            textBox4.Text = clockFace[4].ToString();
+            textBox5.Text = clockFace[5].ToString();
+            textBox6.Text = clockFace[6].ToString();
+            textBox7.Text = clockFace[7].ToString();
+            textBox8.Text = clockFace[8].ToString();
+            textBox9.Text = clockFace[9].ToString();
+            textBox10.Text = clockFace[10].ToString();
+            textBox11.Text = clockFace[11].ToString();
+            textBox0.BackColor = oldIndex == 0 ? Color.Yellow : newIndex == 0 ? Color.Green : Color.White;
+            textBox1.BackColor = Color.White;
+            textBox2.BackColor = Color.White;
+            textBox3.BackColor = oldIndex == 3 ? Color.Yellow : newIndex == 3 ? Color.Green : Color.White;
+            textBox4.BackColor = Color.White;
+            textBox5.BackColor = Color.White;
+            textBox6.BackColor = oldIndex == 6 ? Color.Yellow : newIndex == 6 ? Color.Green : Color.White;
+            textBox7.BackColor = Color.White;
+            textBox8.BackColor = Color.White;
+            textBox9.BackColor = oldIndex == 9 ? Color.Yellow : newIndex == 9 ? Color.Green : Color.White;
+            textBox10.BackColor = Color.White;
+            textBox11.BackColor = Color.White;
+
+
+            txtOldIndex.Text = oldIndex == null ? "---" : oldIndex.ToString();
+            txtOldIndex.BackColor = oldIndex == null ? Color.White : Color.Yellow;
+            txtNewIndex.Text = newIndex == null ? "---" : newIndex.ToString();
+            txtNewIndex.BackColor = newIndex == null ? Color.White : Color.Green;
+        }
+
+        private void SetIndex(byte index)
+        {
+            if (oldIndex == null)
+                oldIndex = index;
+            else if (oldIndex != index && oldIndex.HasValue)
+            {
+                newIndex = index;
+                AllowedMove am = new AllowedMove(oldIndex.Value, newIndex.Value);
+                if (!PerformMove(am, chkForward.Checked))
+                    MessageBox.Show("You are going in circles. Your move was cancelled.");
+
+                newIndex = null;
+                oldIndex = null;
+            }
+            RenderClockFace();
+        }
+
+        public bool PerformMove(AllowedMove am, bool forward)
+        {
+            byte[] originalClockFace = (byte[])clockFace.Clone();
+
+            clockFace.ShiftElement(am, forward);
+            int index = history.FindIndex(l => Enumerable.SequenceEqual(clockFace, l));
+            if (index >= 0)
+            {
+                clockFace = originalClockFace;
+                return false;
+            }
+            else
+            {
+                moves.Add(am);
+                history.Add((byte[])clockFace.Clone());
+                return true;
+            }
+        }
+
+        public bool PerformUndo()
+        {
+            if (moves.Count == 0) return false;
+            history.Remove(history.Last());
+            moves.Remove(moves.Last());
+            clockFace = (byte[])history.Last().Clone() ;
+            newIndex = null;
+            oldIndex = null;
+            return true;
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox0_Click(object sender, EventArgs e)
+        {
+            SetIndex(0);
+        }
+
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            SetIndex(3);
+        }
+
+        private void textBox6_Click(object sender, EventArgs e)
+        {
+            SetIndex(6);
+        }
+
+        private void textBox9_Click(object sender, EventArgs e)
+        {
+            SetIndex(9);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!PerformUndo()) MessageBox.Show("You are at the beginning");
+            else RenderClockFace();
+        }
+
+        public void Restart()
+        {
+            clockFace = new byte[] { 1, 12, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+            history = new List<byte[]>() { (byte[])clockFace.Clone() };
+            moves = new List<AllowedMove>();
+            RenderClockFace();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Restart();
+        }
+        
+        
+    }
+
+
+    public class AllowedMove
+    {
+        public int OldIndex;
+        public int NewIndex;
+        public int Distance
+        {
+            get { return Math.Abs(NewIndex - OldIndex); }
+        }
+        public AllowedMove(int oldIndex, int newIndex)
+        {
+            OldIndex = oldIndex;
+            NewIndex = newIndex;
+
+        }
+    }
+
+
+    public static class Tools
+    {
+        /// <summary>
+        /// http://stackoverflow.com/questions/7242909/moving-elements-in-array-c-sharp
+        /// Modified clockface edition
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="oldIndex"></param>
+        /// <param name="newIndex"></param>
+        public static void ShiftElement<T>(this T[] array, AllowedMove index, bool forward)
+        {
+            int oldIndex = index.OldIndex;
+            int newIndex = index.NewIndex;
+            // TODO: Argument validation
+            if (oldIndex == newIndex)
+            {
+                return; // No-op
+            }
+            T tmp = array[oldIndex];
+
+            if (forward)
+            {
+                if (newIndex < oldIndex)
+                {
+                    // Need to move part of the array "up" to make room
+                    Array.Copy(array, newIndex, array, newIndex + 1, oldIndex - newIndex);
+                }
+                else
+                {
+                    //clockwise movement
+                    T last = array[array.Length - 1];
+                    Array.Copy(array, newIndex, array, newIndex + 1, array.Length - newIndex - 1);
+                    if (oldIndex > 0) Array.Copy(array, 0, array, 1, oldIndex);
+                    array[0] = last;
+
+                    //Array.Copy(array, oldIndex + 1, array, oldIndex, newIndex - oldIndex);
+                }
+            }
+            else
+            {
+                //TODO: dodělat
+                if (newIndex < oldIndex)
+                {
+                    //Counterclockwise movement
+                    T first = array[0];
+                    Array.Copy(array, oldIndex + 1, array, oldIndex, array.Length - oldIndex - 1);
+                    array[array.Length - 1] = first;
+                    if (newIndex > 0) Array.Copy(array, 1, array, 0, newIndex);
+                    //Array.Copy(array, newIndex, array, newIndex + 1, oldIndex - newIndex);
+                }
+                else
+                {
+                    // Need to move part of the array "up" to make room
+                    Array.Copy(array, oldIndex + 1, array, oldIndex, newIndex - oldIndex);
+                }
+
+            }
+            array[newIndex] = tmp;
+        }
+    }
+
+
+}
